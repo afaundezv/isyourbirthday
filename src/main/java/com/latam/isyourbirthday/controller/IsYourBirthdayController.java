45bc90243b8fe6ca64b016618e896582abdc335f
amesfv <sensuke@gmail.com>
2020-05-24 11:43:18 -0400

    refactorización código

--- src/main/java/com/latam/isyourbirthday/controller/IsYourBirthdayController.java
+++ src/main/java/com/latam/isyourbirthday/controller/IsYourBirthdayController.java
@@ -3,10 +3,14 @@ package com.latam.isyourbirthday.controller;
 import com.latam.isyourbirthday.business.IBirthdayBusiness;
 import com.latam.isyourbirthday.business.IsYourBirthdayBusiness;
 import com.latam.isyourbirthday.model.ResponseData;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.text.ParseException;
 
