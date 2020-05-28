42d2c1fd7fea8c52ab1ea1d7105bf6e55c1d66a4
amesfv <sensuke@gmail.com>
2020-05-27 21:31:52 -0400

    refactorización código v2

--- src/main/java/com/latam/isyourbirthday/business/IBirthdayBusiness.java
+++ src/main/java/com/latam/isyourbirthday/business/IBirthdayBusiness.java
@@ -2,9 +2,9 @@ package com.latam.isyourbirthday.business;
 
 import com.latam.isyourbirthday.model.ResponseData;
 
-import java.text.ParseException;
+import java.time.DateTimeException;
 
 public interface IBirthdayBusiness {
 
-    ResponseData execute(String name, String lastName, String motherLastName, String birthday) throws ParseException;
+    ResponseData execute(String name, String lastName, String motherLastName, String birthday) throws DateTimeException;
 }
--- src/main/java/com/latam/isyourbirthday/model/ResponseData.java
+++ src/main/java/com/latam/isyourbirthday/model/ResponseData.java
@@ -3,14 +3,14 @@ package com.latam.isyourbirthday.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 @Getter
 @Setter
 public class ResponseData {
 
     private String nameAndLastName;
-    private Date birthday;
+    private LocalDate birthday;
     private int age;
     private long daysRemainingBirthday;
     private String poem;
